fbdf45630fbb21d38c17c97506577d36694b1519
PranjeetGoswami <prgoswami2020@gmail.com>
2025-02-20 23:52:35 +0530

    add to repo

--- A Project/src/index.js
+++ A Project/src/index.js
@@ -2,18 +2,18 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({
-    path: "./env"
-})
+  path: "./env",
+});
 
-connectDB();
-
-
-
-
-
-
-
-
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB Connection Failed !!!", err);
+  });
 
 /*
 // IFFE function
