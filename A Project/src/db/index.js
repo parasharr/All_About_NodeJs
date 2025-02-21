ca487b1f3e36306ec98a169b13dcc2da856dd430
PranjeetGoswami <prgoswami2020@gmail.com>
2025-02-21 08:29:55 +0530

    add to repo

--- A Project/src/db/index.js
+++ A Project/src/db/index.js
@@ -2,13 +2,17 @@ import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
-    try {
-        const connectionBase = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
-        console.log(`\n MongoDB Connected !! DB HOST: ${connectionBase.connection.host}`)
-    } catch (error) {
-        console.log("MongoDB connection ERROR", error);
-        process.exit(1);
-    }
-}
+  try {
+    const connectionBase = await mongoose.connect(
+      `${process.env.MONGODB_URL}/${DB_NAME}`
+    );
+    console.log(
+      `\n MongoDB Connected !! DB HOST: ${connectionBase.connection.host}`
+    );
+  } catch (error) {
+    console.log("MongoDB connection ERROR", error);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
